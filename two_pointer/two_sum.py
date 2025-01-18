2a513cc608b5ff15c3e788767331bec83ff33f74
iDavi <odavi20527@gmail.com>
2025-01-18 17:06:21 -0300

    add two sum

--- two_pointer/two_sum.py
+++ two_pointer/two_sum.py
@@ -6,3 +6,22 @@ class Solution:
             if val in to_find:
                 return [to_find[val], i]
             to_find[target-val] = i
+
+
+#sorted array:
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_size = len(nums)
+        j = nums_size-1
+        i = 0
+        while(i < j):
+            num1 = nums[i]
+            num2 = nums[j]
+            if(num1+num2 == target):
+                return [i+1,j+1]
+            if(num1+num2 > target):
+                j -= 1
+            else:
+                i += 1
+        
